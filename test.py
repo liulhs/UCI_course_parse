bdff4e72865dadd74c14864f0a23be2ad57098b3
liulhs <liulhs1998@gmail.com>
2018-12-10 16:21:00 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import mechanicalsoup
+url = 'https://www.reg.uci.edu/perl/WebSoc'
+
+browser = mechanicalsoup.StatefulBrowser(
+    soup_config={'features': 'lxml'},
+    raise_on_404=True,
+)
+
+browser.open(url)
+##browser.select_form('form[action="{}"]'.format(url))
+##browser["CourseCodes"] = code
+##
+##response = browser.submit_selected()
+##
+##soup = BeautifulSoup(response.text, 'lxml')
+##td = soup.find_all(attrs={'bgcolor':'#D5E5FF'})[-1]
+##
+
