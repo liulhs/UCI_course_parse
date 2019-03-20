17651835e48a67095fafea54a05e9a1f26230cf0
Jason <37915983+liulhs@users.noreply.github.com>
2019-03-20 12:43:20 -0700

    ZXL / QSY

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
-import sched, time
-starttime=time.time()
-while True:
-	print('hey')
-	time.sleep(2)
+import smtplib, ssl
+
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "jasonliuofficial@gmail.com"
+receiver_email = "shiyuq@uci.edu"
+password = "112131415161lhs"
+message = """\
+Subject: Hi there
+You are Now on the watchlist of the following class(es):
+34210
+34211
+34212
+34213
+34214
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
---
+++ zxl.py
@@ -0,0 +1,80 @@
+from bs4 import BeautifulSoup
+import mechanicalsoup
+
+import time
+import sched
+
+import smtplib
+try:
+    from email.MIMEText import MIMEText
+except:
+    from email.mime.text import MIMEText
+try:
+    from email.MIMEMultipart import MIMEMultipart
+except:
+    from email.mime.multipart import MIMEMultipart
+
+def is_full (code):
+
+    url = 'https://www.reg.uci.edu/perl/WebSoc'
+
+    browser = mechanicalsoup.StatefulBrowser(
+        soup_config={'features': 'lxml'},
+        raise_on_404=True,
+    )
+    browser.open(url)
+    browser.select_form('form[action="{}"]'.format(url))
+    browser["CourseCodes"] = code
+
+    response = browser.submit_selected()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    td = soup.find_all(attrs={'bgcolor':'#D5E5FF'})[-1]
+    # print(soup.text.find("FULL")!= -1)
+    # return td.text.strip() == 'FULL'
+    return soup.text.find("FULL")!= -1
+
+def send_email (code):
+    subject = 'UCI Course Master: The course ({}) you are watching is not full anymore'.format(code)
+    body = """\
+    <html>
+    <head></head>
+    <body>
+    <p>
+       The course {} has either an open spot or opened a waitlist.
+    </p>
+    </body>
+    </html>
+    """.format(code)
+
+    fromaddr = "jasonliuofficial@gmail.com"
+    toaddr = ["haosonl@uci.edu","liulhs1998@gmail.com"]
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'html'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(fromaddr, "112131415161lhs")
+
+    for to in toaddr:
+        msg['To'] = to
+        server.sendmail(fromaddr, to, msg.as_string())
+        print (str("Email sent to \"" + to + "\"."))
+
+    server.quit()
+
+if __name__ == '__main__':
+    course_list = [70375，70360，70365]
+    sent = False
+    while not sent:
+        for code in course_list:
+            if not is_full(code):
+                send_email(code)
+                sent = True
+        if not sent:
+            print('ALLFULL')
+        time.sleep(300)
