81dea356cbc9227ac8cbff7c02782e1e279711c8
Jason <37915983+liulhs@users.noreply.github.com>
2019-05-01 01:34:56 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,14 @@ import smtplib, ssl
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
 sender_email = "jasonliuofficial@gmail.com"
-receiver_email = "shiyuq@uci.edu"
+receiver_email = "qifanz1@uci.edu"
 password = "112131415161lhs"
+custom_message = """The following girls:
+lzq
+wcy
+gth
+have already started liking you, congrats!!!!!!!!
+"""
 message = """\
 Subject: Hi there
 You are Now on the watchlist of the following class(es):
@@ -21,4 +27,5 @@ with smtplib.SMTP(smtp_server, port) as server:
     server.starttls(context=context)
     server.ehlo()  # Can be omitted
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, custom_message)
+print("Message Sent!")
